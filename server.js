9642deba7b99c6f2337d65ed53f22db98f61ff54
Julio Cesar Diaz M <juliocm17@gmail.com>
2018-01-06 19:43:59 -0500

    Solve bugs in Holidays datatable

--- server.js
+++ server.js
@@ -9,15 +9,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
 app.engine('hbs', hbs.express4({
-    partialsDir: __dirname + '/views'
+    defaultLayout: __dirname + '/views/layouts/main.hbs'
 }))
 
 app.use(express.static(path.join(__dirname, 'node_modules')));
-
 app.use('/', index);
 
 app.set('port', (process.env.port || 3000))
-
 app.listen(app.get('port'), function(){
     console.log('Server running on port ' + app.get('port'))
 })
